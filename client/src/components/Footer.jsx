830e0fe5143dcd6cbcab68fdf32556de77af8843
lavanayakhosla <144594627+lavanayakhosla@users.noreply.github.com>
2025-02-23 14:34:41 +0530

    Update Footer.jsx

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 const Footer = () => (
-  <div className="w-full flex md:justify-center justify-between items-center flex-col p-4 ">
+  <div className="w-full flex bg-black text-white md:justify-center justify-between items-center flex-col p-4 ">
   <div className="sm:w-[90%] w-full h-[0.25px] bg-gray-400 mt-5 " />
 
     <div className="sm:w-[90%] w-full flex justify-between items-center mt-3">
